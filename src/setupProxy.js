349f21da10f3061fdec7d3759389536c3166e169
Cai Borui <756321586@qq.com>
2023-04-29 18:17:10 +0100

    import ffmpeg.wasm, create setupproxy for cors to solve sharedArrayBuffer is not defined

---
+++ src/setupProxy.js
@@ -0,0 +1,10 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+
+module.exports = (app) => {
+    app.use((_, res, next) => {
+      res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+      res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+      next();
+    });
+  };
